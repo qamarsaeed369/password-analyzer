9006d5de0bcff7a6de6863c41e4efd1a9ac5fdfd
Qamar Saeed <qamarsaeed369@gmail.com>
2026-01-09 10:02:47 +0000

    Update code

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Exclude TensorFlow.js from server-side bundle
+    // This prevents 500 errors when TensorFlow tries to initialize in Node.js/serverless
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        '@tensorflow/tfjs': 'commonjs @tensorflow/tfjs',
+        '@tensorflow/tfjs-node': 'commonjs @tensorflow/tfjs-node',
+      });
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
